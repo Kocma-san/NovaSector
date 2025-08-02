9a615f03e816dec4f67d88663dda774caec2b889
Kocma-san <kocmasan@gmail.com>
2025-08-03 05:14:11 +0700

    blep

--- tools/pull_request_hooks/autoChangelog.test.js
+++ tools/pull_request_hooks/autoChangelog.test.js
@@ -5,17 +5,17 @@ import { parseChangelog } from "./changelogParser.js";
 assert.equal(
   changelogToYml(
     parseChangelog(`
-			My cool PR!
+			My cool PR! weh
 			:cl: DenverCoder9
 			add: Adds new stuff
 			add: Adds more stuff
 			/:cl:
-		`),
+		`)
   ),
 
   `author: "DenverCoder9"
 delete-after: True
 changes:
   - rscadd: "Adds new stuff"
-  - rscadd: "Adds more stuff"`,
+  - rscadd: "Adds more stuff"`
 );
